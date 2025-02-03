509a872ccd3e70a631e64070921a0f46dc2358b8
saugat46 <saugatsharma610@gmail.com>
2025-02-03 19:51:22 +0545

    added testamonial and contact section

--- resources/views/Layout/navbar.blade.php
+++ resources/views/Layout/navbar.blade.php
@@ -1,4 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top" data-spy="affix" data-offset-top="0">
+    <div class="container">
 
     <a class="navbar-brand ms-1 me-4" href="/" style="letter-spacing: 0.4em; font-family: 'Pacifico'; font-size: 2em;">
         SAUGAT
@@ -34,5 +35,7 @@
                 <a class="nav-link fs-5 py-3 ms-2 {{ Request::is('login') ? 'active' : '' }}" href="{{ url('/login') }}">Login</a>
                 </li> -->
         </ul>
+    </div>
+            
     </div>
 </nav>
---
+++ resources/views/section.blade.php
@@ -0,0 +1,350 @@
+<!-- Sections -->
+<section id="home">
+    <div class="container home">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="infos mb-4 mb-md-0">
+                    <h6 class="subtitle">hello, I'm</h6>
+                    <h6 class="title">Saugat Danai Sharma</h6>
+                    <p class="subheading">Web Developer</p>
+
+                    <div class="buttons pt-3">
+                        <button class="btn btn-primary rounded">HIRE ME</button>
+                        <button class="btn btn-dark rounded">DOWNLOAD CV</button>
+                    </div>
+
+                    <div class="socials mt-4 d-flex gap-2">
+                        <a class="btn custom-btn-outline social-item" href="javascript:void(0)">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                        <a class="btn custom-btn-outline social-item" href="javascript:void(0)">
+                            <i class="fab fa-google"></i>
+                        </a>
+                        <a class="btn custom-btn-outline social-item" href="javascript:void(0)">
+                            <i class="fab fa-github"></i>
+                        </a>
+                        <a class="btn custom-btn-outline social-item" href="javascript:void(0)">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 d-none d-md-block svg">
+                <dotlottie-player src="https://lottie.host/1bf5dd3b-127a-45b1-8c10-5b6da86ea433/KfxDRFOIl0.lottie"
+                    background="transparent" speed="1" style="width: 100%; height: auto;" loop autoplay>
+                </dotlottie-player>
+            </div>
+            <div class="widget mt-4 mt-md-0 ">
+                <div class="widget-item">
+                    <h1>500</h1>
+                    <p>Happy Clients</p>
+                </div>
+                <div class="widget-item">
+                    <h1>20</h1>
+                    <p>Project Completed</p>
+                </div>
+                <div class="widget-item">
+                    <h1>60</h1>
+                    <p>Award Won</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="about">
+    <div class="container">
+        <div class="row text-md-left">
+            <div class="col-md-3">
+                <img src="assets/profileImg/profileimg.jpg" alt="About Me" class="profile-img">
+            </div>
+            <div class="pl-md-4 col-md-9">
+                <h6 class="name">Saugat Danai Sharma</h6>
+                <p class="subheading">Web Developer</p>
+
+                <div class="description">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, pariatur, aperiam aut
+                        autem
+                        voluptas odit. Odio ducimus delectus totam sed aliquam sequi praesentium mollitia, illum
+                        repudiandae quidem quod, magni magnam.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, eius, nam. Quo praesentium
+                        qui
+                        temporibus voluptatum, facilis aliquid eligendi fugiat beatae neque inventore non. Laborum
+                        repellendus consequatur ullam voluptatum asperiores.
+                    </p>
+                </div>
+                <div class="buttons pt-3">
+                    <button class="btn btn-primary rounded">DOWNLOAD CV</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="service">
+    <div class="container mb-5">
+        <div class="row">
+            <div class="col">
+                <h6 class="subheading text-center">Service</h6>
+                <h6 class="subname text-center mb-4">What I Do</h6>
+                <div class="description text-center">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In alias dignissimos.
+                    </p>
+                    <p>
+                        rerum commodi corrupti, temporibus non quam.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-sm-6 col-md-3 mb-4">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <i class="fas fa-code fa-2x mb-2"></i>
+                        <h5 class="service-name">Web Development</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 mb-4">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <i class="fas fa-cogs fa-2x mb-2"></i>
+                        <h5 class="service-name">DevOps</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 mb-4">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <i class="fas fa-bug fa-2x mb-2"></i>
+                        <h5 class="service-name">Bug Bounty</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 mb-4">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <i class="fas fa-shield-alt fa-2x mb-2"></i>
+                        <h5 class="service-name">Security Assessment</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <h6 class="subheading text-center">Skills</h6>
+        <h6 class="subname text-center mb-4">Why Choose Me</h6>
+        <div class="description text-center">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In alias dignissimos.
+                <br>
+                rerum commodi corrupti, temporibus non quam.
+            </p>
+        </div>
+        <div class="row text-left">
+            <div class="col-sm-6 mt-5">
+                <h6 class="mb-3">Web Development</h6>
+                <div class="progress">
+                    <div class="progress-bar bg-primary" role="progressbar" style="width: 99%;" aria-valuenow="25"
+                        aria-valuemin="0" aria-valuemax="100"> </div>
+                </div>
+            </div>
+            <div class="col-sm-6 mt-5">
+                <h6 class="mb-3">DevOps</h6>
+                <div class="progress">
+                    <div class="progress-bar bg-primary" role="progressbar" style="width: 80%;" aria-valuenow="25"
+                        aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+            <div class="col-sm-6 mt-5">
+                <h6 class="mb-3">Bug Bounty</h6>
+                <div class="progress">
+                    <div class="progress-bar bg-primary" role="progressbar" style="width: 77%;" aria-valuenow="25"
+                        aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+            <div class="col-sm-6 mt-5">
+                <h6 class="mb-3">Security Assessment</h6>
+                <div class="progress">
+                    <div class="progress-bar bg-primary" role="progressbar" style="width: 89%;" aria-valuenow="25"
+                        aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+        </div>
+</section>
+
+<section id="portfolio">
+    <div class="container">
+        <h6 class="subheading text-center">Services</h6>
+        <h6 class="subname text-center mb-4">What I do</h6>
+        <div class="description text-center">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In alias dignissimos.
+            </p>
+            <p>
+                rerum commodi corrupti, temporibus non quam.
+            </p>
+        </div>
+        <div class="row text-left">
+            <div class="col-sm-6 mt-5">
+                <h6 class="mb-3">Web Development</h6>
+                <div class="progress">
+                    <div class="progress-bar bg-primary" role="progressbar" style="width: 99%;" aria-valuenow="25"
+                        aria-valuemin="0" aria-valuemax="100"> </div>
+                </div>
+            </div>
+            <div class="col-sm-6 mt-5">
+                <h6 class="mb-3">DevOps</h6>
+                <div class="progress">
+                    <div class="progress-bar bg-primary" role="progressbar" style="width: 80%;" aria-valuenow="25"
+                        aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+            <div class="col-sm-6 mt-5">
+                <h6 class="mb-3">Bug Bounty</h6>
+                <div class="progress">
+                    <div class="progress-bar bg-primary" role="progressbar" style="width: 77%;" aria-valuenow="25"
+                        aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+            <div class="col-sm-6 mt-5">
+                <h6 class="mb-3">Security Assessment</h6>
+                <div class="progress">
+                    <div class="progress-bar bg-primary" role="progressbar" style="width: 89%;" aria-valuenow="25"
+                        aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+        </div>
+</section>
+
+<section id="testimonial">
+    <div class="container">
+        <h6 class="subheading text-center">Testimonial</h6>
+        <h6 class="subname text-center mb-4">What People Say About Me</h6>
+        <div class="description text-center">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In alias dignissimos.<br>Rerum commodi
+                corrupti, temporibus non quam.</p>
+        </div>
+        <div id="testimonialCarousel d-flex" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
+            <div class="carousel-inner d-flex">
+                <!-- First Slide -->
+                <div class="carousel-item active">
+                    <div class="custom-card">
+                        <img src="assets/profileimg/profileImg.jpg" class="testimonial-img" alt="Profile Image">
+                        <p class="testimonial-text">I'm in love with this template. It's so easy to use and customize. I
+                            love the color.</p>
+                        <h1 class="testimonial-title">John Doe</h1>
+                        <h1 class="testimonial-sub-title">Web Developer</h1>
+                    </div>
+                </div>
+                <!-- Second Slide -->
+                <div class="carousel-item">
+                    <div class="custom-card">
+                        <img src="assets/profileimg/profileImg.jpg" class="testimonial-img" alt="Profile Image">
+                        <p class="testimonial-text">This is another testimonial. The template is fantastic and easy to
+                            use.</p>
+                        <h1 class="testimonial-title">John Doe</h1>
+                        <h1 class="testimonial-sub-title">Project Manager</h1>
+                    </div>
+                </div>
+                <!-- Third Slide -->
+                <div class="carousel-item">
+                    <div class="custom-card">
+                        <img src="assets/profileimg/profileImg.jpg" class="testimonial-img" alt="Profile Image">
+                        <p class="testimonial-text">Great template! It has helped me a lot in my projects.</p>
+                        <h1 class="testimonial-title">John Doe</h1>
+                        <h1 class="testimonial-sub-title">QA</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+<section id="blog">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h6 class="subheading text-center">Service</h6>
+                <h6 class="subname text-center mb-4">What I Do</h6>
+                <div class="description text-center">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In alias dignissimos.
+                    </p>
+                    <p>
+                        rerum commodi corrupti, temporibus non quam.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-sm-6 col-md-3 mb-4">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <i class="fas fa-code fa-2x mb-2"></i>
+                        <h5 class="service-name">Web Development</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 mb-4">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <i class="fas fa-cogs fa-2x mb-2"></i>
+                        <h5 class="service-name">DevOps</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 mb-4">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <i class="fas fa-bug fa-2x mb-2"></i>
+                        <h5 class="service-name">Bug Bounty</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 mb-4">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <i class="fas fa-shield-alt fa-2x mb-2"></i>
+                        <h5 class="service-name">Security Assessment</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="contact">
+    <div class="container">
+        <h6 class="subheading text-center">Contact</h6>
+        <h6 class="subname text-center mb-4">Get In Touch With Me</h6>
+        <div class="description text-center">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In alias dignissimos.<br>Rerum commodi
+                corrupti, temporibus non quam.</p>
+            <div class="contact-card">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h6 class="subheading d-flex">Available 24/7</h6>
+                        <h6 class="subname text-center mb-4 d-flex">Get In Touch</h6>
+                        <div class="form-group">
+                            <input type="email" class="form-control mb-3" id="email" aria-describedby="emailHelp"
+                                   placeholder="Enter email" required>
+                            <input type="text" class="form-control mb-3" id="subject" placeholder="Subject" required>
+                            <textarea name="contact-message" id="" cols="30" rows="5" class="form-control mb-3"
+                                      placeholder="Message"></textarea>
+                            <button type="submit" class="btn btn-primary btn-block rounded w-lg">Send Message</button>
+                        </div>
+                    </div>
+                    <div class="col-md-6 d-none d-md-block">
+                        <div class="contact-info">
+                            <h6>Address</h6>
+                            <p>1234 Street Name, City Name, United States</p>
+                            <h6>Email</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
